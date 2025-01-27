c59fbf1cd305a5806c4c64c818dbbb8bcd0edcb7
Jolo Tolentino <jolo@Jolos-MacBook-Pro.local>
2025-01-27 10:28:49 +0700

    loading data

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { createRoot } from "react-dom/client";
 import PokemonList from "./pokemon-list";
 
 const container = document.getElementById("root");
+// debugger
 const root = createRoot(container);
 root.render(<PokemonList />);
--- src/pokemon-list.jsx
+++ src/pokemon-list.jsx
@@ -1,7 +1,78 @@
-// You can retrieve the pokemons by calling the following API
-// Make sure to replace limit and offset with the appropriate values
-// https://pokeapi.co/api/v2/pokemon?limit=5&offset=0
+import React, { useState, useEffect } from "react";
 
-const PokemonList = () => {};
+
+
+const PokemonList = () => {
+    const [pokemonList, setPokemonList] = useState([]); // To store the list of Pokémon
+    const [err, setErr] = useState(null); // To handle errors
+    const [offset,setOffset] = useState(0);  
+    let limit = 5;
+
+
+    const getPokeMeta = async(url) => {
+        let data;
+        const response = await fetch(url);
+        if (!response.ok){
+            throw new Error(`Error : ${response.status}`)
+        }
+
+        data = await response.json()
+        return {
+            abilities: data.abilities,
+            sprites: data.sprites.other['official-artwork'].front_default
+        }
+    }
+
+
+    const getPokemons = async () => {
+        let pokemons;
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+            if (!response.ok) {
+                throw new Error(`Error ${response.status}`);
+            }
+            const data = await response.json(); 
+            const pokemonData = data.results; 
+            pokemons = await Promise.all (pokemonData.map( async (pokemon)=> {
+                return {name :pokemon.name.trim(), 
+                        metadata: await getPokeMeta(pokemon.url)
+                    }
+                })); 
+            // debugger
+            return pokemons;
+        } catch (error) {
+            setErr(error.message);
+            return [] 
+        }
+    };
+
+
+    const getNextPokemons = () =>{
+        setOffset(offset+limit);
+    }
+
+
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const pokemons = await getPokemons(); // Wait for the async function
+            setPokemonList(pokemons); // Update the state with fetched data
+
+        };
+        fetchData(); 
+    }, [offset]); 
+
+    return (
+        <div>
+            <h1>Pokemon List</h1>
+            <ul>
+                {pokemonList.map((pokemon,index) => (
+                    <li key={index}>{pokemon.name} <img src = {pokemon.metadata.sprites}/></li> 
+                ))}
+            </ul>
+            <button onClick={getNextPokemons}>Load more</button>
+        </div>
+    );
+};
 
 export default PokemonList;
